038b5ef55dad3e10e15042a14a309de7c66da00c
KANG\b2000 <hanqjun2660@gmail.com>
2023-10-10 16:54:11 +0900

    p.139

--- chap04/src/IntStack.java
+++ chap04/src/IntStack.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class IntStack {
 
     /**
@@ -55,4 +53,52 @@ public class IntStack {
         }
         return stk[ptr - 1];
     }
+
+    // 스택 비움
+    public void clear() {
+        ptr = 0;
+    }
+
+    // 스택에서 x를 찾아 인덱스(없으면 -1)를 반환
+    public int indexOf(int x) {
+        for(int i = ptr - 1; i >= 0; i--) {     // 스택의 꼭대기부터 선형검색
+            if(stk[i] == x) {
+                return i;       // 검색 성공
+            }
+        }
+        return -1;              // 검색 실패
+    }
+
+    // 스택의 용량을 반환
+    public int getCapacity() {
+        return capacity;
+    }
+
+    // 스택에 쌓여있는 데이터 갯수를 반환
+    public int size() {
+        return ptr;
+    }
+
+    // 스택이 비어있는지
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+
+    // 스택이 가득 찼는지
+    public boolean isFull() {
+        return ptr >= capacity;
+    }
+
+    // 스택안의 모든 데이터를 바닥 -> 꼭대기 순서로 출력
+    public void dump() {
+        if(ptr <= 0) {
+            System.out.println("스택이 비어있습니다.");
+        } else {
+            for(int i = 0; i < ptr; i++) {
+                System.out.print(stk[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
