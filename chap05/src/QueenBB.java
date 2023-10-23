dd9fb085b505f9f489c0ac313a9a90ede55480ef
KANG\b2000 <hanqjun2660@gmail.com>
2023-10-23 14:40:54 +0900

    p.195

--- chap05/src/QueenBB.java
+++ chap05/src/QueenBB.java
@@ -12,14 +12,14 @@ public class QueenBB {
 
     static void set(int i) {
         for(int j = 0; j < 8; j++) {
-            if(!flag[j]) {
+            if(!flag[j]) {                      // 아직 행에 퀸이 배치가 안되어 있을때
                 pos[i] = j;
-                if(i == 7) {
+                if(i == 7) {                    // i가 7이면 모든 퀸의 배치가 끝났으므로 출력
                     print();
                 } else {
-                    flag[j] = true;
-                    set(i + 1);
-                    flag[j] = false;
+                    flag[j] = true;             // j행에 퀸 배치가 끝나면 true로 바꿈
+                    set(i + 1);                 // set 메서드 재귀호출 -> 다음 행에 퀸을 배치
+                    flag[j] = false;            // 재귀호출한 메서드 실행이 끝나면 퀸을 j행에서 제거하기 위해 flag에 false를 대입
                 }
             }
         }
